a3d43c1f9754e438a521d8fcc3af3fc1b2165248
Wald14 <lukewald@hotmail.com>
2023-11-08 21:39:07 -0600

    Updated server.js file

--- server.js
+++ server.js
@@ -1,32 +1,37 @@
+const path = require('path')
 const express = require('express');
-const app = express();
-
-// Add if using handlebars
+const session = require('express-session')
 const exphbs = require('express-handlebars');
+const routes = require('./routes')
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 const hbs = exphbs.create({});
+
+const sess = {
+  secret: 'xD0gPEcYLsWS8f',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess))
+
 app.engine('handlebars', hbs.engine); // express: here's a new rendering engine
 app.set('view engine', 'handlebars'); // express: make handlebars the default renderer
 
-const path = require('path')
-const routes = require('./routes')
-const sequelize = require('./config/connection');
-
-// Import models to sync with database
-const models = require('./models')
-
-const PORT = process.env.PORT || 3001;
-
-app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
 
-// Home page route
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-})
-
-// All other routes are directed elsewhere
-app.use('*', routes);
+app.use(routes);
 
 
 const okToSync = (process.env.NODE_ENV === 'production') ? false : true;
